f6b9997b1a0dfb975c4e0baada726ca109276c0f
Bhik95 <32757704+Bhik95@users.noreply.github.com>
2019-11-28 17:59:41 +0100

    Colored Crystal Base Mechanic (Prefabs)

---
+++ Assets/ColoredCrystalPuzzleManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoredCrystalPuzzleManager : MonoBehaviour
+{
+    private bool _active_red = false;
+    private bool _active_yellow = false;
+    private bool _active_blue = false;
+
+    public event Action<PrimaryColor, bool> StateChanged;
+
+    public static ColoredCrystalPuzzleManager Instance
+    {
+        get
+        {
+            if (!_instance)
+                _instance = FindObjectOfType<ColoredCrystalPuzzleManager>();
+
+            return _instance;
+        }
+
+}
+
+    private static ColoredCrystalPuzzleManager _instance;
+
+    private void Awake()
+    {
+        if (_instance)
+        {
+            Debug.LogError("Singleton violated.");
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public bool ActiveRed
+    {
+        get
+        {
+            return _active_red;
+        }
+        set
+        {
+            _active_red = value;
+            StateChanged?.Invoke(PrimaryColor.RED, value);
+        }
+    }
+    
+    public bool ActiveBlue
+    {
+        get
+        {
+            return _active_blue;
+        }
+        set
+        {
+            _active_blue = value;
+            StateChanged?.Invoke(PrimaryColor.BLUE, value);
+        }
+    }
+    
+    public bool ActiveYellow
+    {
+        get
+        {
+            return _active_yellow;
+        }
+        set
+        {
+            _active_yellow = value;
+            StateChanged?.Invoke(PrimaryColor.YELLOW, value);
+        }
+    }
+    
+}
---
+++ Assets/PrimaryColorCrystal.cs
@@ -0,0 +1,67 @@
+﻿#pragma warning disable 649
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public enum PrimaryColor
+{
+    RED, BLUE, YELLOW
+}
+
+public class PrimaryColorCrystal : Interactible
+{
+
+
+    [SerializeField] private PrimaryColor _color;
+
+    [SerializeField] private GameObject _brazier;
+    [SerializeField] private FMODUnity.StudioEventEmitter _on_lit_sound;
+
+    private void Start()
+    {
+        ColoredCrystalPuzzleManager.Instance.StateChanged += OnManagerStateChanged;
+    }
+
+    private void OnManagerStateChanged(PrimaryColor color, bool value)
+    {
+        if(color == _color)
+            DisplayStatus(value);
+    }
+
+    protected override void OnActivate()
+    {
+        switch (_color)
+        {
+            case PrimaryColor.BLUE:
+                ColoredCrystalPuzzleManager.Instance.ActiveBlue = !ColoredCrystalPuzzleManager.Instance.ActiveBlue;
+                break;
+            case PrimaryColor.RED:
+                ColoredCrystalPuzzleManager.Instance.ActiveRed = !ColoredCrystalPuzzleManager.Instance.ActiveRed;
+                break;
+            case PrimaryColor.YELLOW:
+                ColoredCrystalPuzzleManager.Instance.ActiveYellow = !ColoredCrystalPuzzleManager.Instance.ActiveYellow;
+                break;
+        }
+    }
+
+    private void DisplayStatus(bool status)
+    {
+        _brazier.SetActive(status);
+        if (status)
+        {
+            _on_lit_sound.Play();
+        }
+    }
+
+    protected override void OnPlayerFar()
+    {
+        //Debug.Log("TODO: Player is faraway (use to add juice)");
+    }
+
+    protected override void OnPlayerNearby()
+    {
+        //Debug.Log("TODO: Player nearby  (use to add juice)");
+    }
+}
