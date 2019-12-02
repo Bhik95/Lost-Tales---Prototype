22d880a5acae960fca20eb3686e5c8ba93ce4e72
Bhik95 <32757704+Bhik95@users.noreply.github.com>
2019-12-02 20:48:12 +0100

    Crystal prefabs

---
+++ Assets/CrystalPuzzleSpriteHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalPuzzleSpriteHandler : MonoBehaviour
+{
+    [SerializeField] private Sprite Activated;
+    [SerializeField] private Sprite Deactivated;
+
+    private SpriteRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetCrystalSprite(bool state)
+    {
+        if (state)
+        {
+            _renderer.sprite = Activated;
+        }
+        else
+        {
+            _renderer.sprite = Deactivated;
+        }
+    }
+}
