797923fa6ebaf7a90f0c18bbfef398fb02c0503d
Bhik95 <32757704+Bhik95@users.noreply.github.com>
2019-12-03 15:56:52 +0100

    Added bonfire to MainScene + BonfireScript never tested

---
+++ Assets/Bonfire.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : Interactible
+{
+
+    [SerializeField] private FMODUnity.StudioEventEmitter _interact_success;
+    [SerializeField] private FMODUnity.StudioEventEmitter _interact_fail;
+
+    [SerializeField] private GameObject _flame_fx_green;
+    [SerializeField] private GameObject _flame_fx_yellow;
+    [SerializeField] private GameObject _flame_fx_purple;
+
+    protected override void OnActivate()
+    {
+        if (PlayerStatus.Instance.HasBigFlame)
+        {
+            _interact_success.Play();
+            var flamesDatas = PlayerStatus.Instance.gameObject.GetComponentsInChildren<SacredFlameData>();
+
+            if(flamesDatas != null)
+            {
+                for(int i = 0; i < flamesDatas.Length; i++)
+                {
+                    switch (flamesDatas[i].FlameType)
+                    {
+                        case FlameType.GREEN:
+                            _flame_fx_green.SetActive(true);
+                            break;
+                        case FlameType.YELLOW:
+                            _flame_fx_yellow.SetActive(true);
+                            break;
+                        case FlameType.PURPLE:
+                            _flame_fx_purple.SetActive(true);
+                            break;
+
+                    }
+                    ParticleSystem ps = flamesDatas[i].GetComponent<ParticleSystem>();
+                    ps.Stop();
+                    Destroy(ps.gameObject, ps.main.duration);
+                }
+            }
+        }
+        else
+        {
+            _interact_fail.Play();
+        }
+    }
+
+    protected override void OnPlayerFar()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    protected override void OnPlayerNearby()
+    {
+        //throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/SacredFlameData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlameType
+{
+    GREEN=0, YELLOW=1, PURPLE=2
+}
+
+public class SacredFlameData : MonoBehaviour
+{
+    [SerializeField] private FlameType _flameType;
+    public FlameType FlameType => _flameType;
+}
