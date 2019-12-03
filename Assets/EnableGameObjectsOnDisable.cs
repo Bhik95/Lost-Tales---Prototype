a29846c74d8a2c953eb2960ca942fa03a7437318
Bhik95 <32757704+Bhik95@users.noreply.github.com>
2019-12-03 22:08:01 +0100

    Crystal puzzle is now on

---
+++ Assets/EnableGameObjectsOnDisable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGameObjectsOnDisable : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _to_enable;
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _to_enable.Length; i++)
+        {
+            _to_enable[i].SetActive(true);
+        }
+    }
+}
