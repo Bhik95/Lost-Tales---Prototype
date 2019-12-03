24e6b17b01b7813ec11a134d6ff009b776653fa3
Per Kristian Sørensen <perkshs@gmail.com>
2019-12-03 10:10:21 +0100

    torches an ai

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -5,25 +5,8 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour
 {
     public static PlayerStatus Instance;
-    GameObject FireParticleEffect;
-    public bool HasBigFlame
-    {
-        get
-        {
-            return m_hasBigFlame;
-        }
-        set
-        {
-            if (FireParticleEffect)
-            {
-                FireParticleEffect.SetActive(value);
-            }
-            m_hasBigFlame = value;
-        }
-    }
-
-    bool m_hasBigFlame;
-
+    public bool HasBigFlame;
+    public Vector3 flameSize;
     private void Awake()
     {
         Instance = this;
---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : Interactible
+{
+    [SerializeField] private GameObject _paintSprite;
+    [SerializeField] private GameObject _brazier;
+    [SerializeField] private FMODUnity.StudioEventEmitter _on_lit_sound;
+    protected override void OnActivate()
+    {
+        _brazier.SetActive(true);
+        _on_lit_sound.Play();
+        if (PlayerStatus.Instance.HasBigFlame)
+        {
+            _paintSprite.transform.localScale = new Vector3(1.2f,1.2f,1);
+        }
+        else
+        {
+            _paintSprite.transform.localScale = new Vector3(.2f, .2f, 1);
+        }
+    }
+
+    protected override void OnPlayerFar()
+    {
+        //Debug.Log("TODO: Player is faraway (use to add juice)");
+    }
+
+    protected override void OnPlayerNearby()
+    {
+        //Debug.Log("TODO: Player nearby  (use to add juice)");
+    }
+}
