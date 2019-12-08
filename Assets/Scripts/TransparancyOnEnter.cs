293788f084eff13ea964ae211abb6ef417f09191
Im-Alexandra <sasa.labusova@gmail.com>
2019-12-08 20:53:13 +0100

    adding foliage and transparencuy on enter (all on scene prefabs)

---
+++ Assets/Scripts/TransparancyOnEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class TransparancyOnEnter : MonoBehaviour
+{
+    public float transp = .5f;
+
+    float curT = 1;
+    private void Update()
+    {
+        Color c = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = Color.Lerp(c, new Color(c.r, c.g, c.b, curT), Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == StaticVariables.Tags.Player)
+        {
+            curT = transp;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == StaticVariables.Tags.Player)
+        {
+            curT = 1;
+        }
+    }
+}
