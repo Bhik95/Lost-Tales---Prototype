cfa9c29d5b7241061765853ae669105b3aaa5ddb
Bhik95 <32757704+Bhik95@users.noreply.github.com>
2019-12-10 20:27:40 +0100

    Storyboard timeline (sounds to be added)

--- Assets/Scripts/ParallaxHandler.cs
+++ Assets/Scripts/ParallaxHandler.cs
@@ -7,8 +7,10 @@ public class ParallaxHandler : MonoBehaviour
 {
 
     [SerializeField] private RectTransform[] ParallaxElements;
+    [SerializeField] private Image[] _fadingElements;
     [SerializeField] private float _amplitude = 1;
     [SerializeField] private float _frequency = 1;
+    [SerializeField] private float _alpha = 1;
 
     void Update()
     {
@@ -16,6 +18,12 @@ public class ParallaxHandler : MonoBehaviour
         for(int i = 0; i < ParallaxElements.Length; i++){
             z = ParallaxElements[i].localPosition.z;
             ParallaxElements[i].localPosition = new Vector3(_amplitude / -z * Mathf.Sin(_frequency * Time.time), 0, z);
+        }
+        float interval = _fadingElements.Length;
+        for(int i = 0; i < _fadingElements.Length; i++)
+        {
+            float _alpha_i = Mathf.Clamp01(interval * _alpha - i);
+            _fadingElements[i].color = new Color(1f, 1f, 1f, _alpha_i);
         }
     }
 }
