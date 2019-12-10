b51b35574828c089c84e025784ebfb3731709f73
Bhik95 <32757704+Bhik95@users.noreply.github.com>
2019-12-10 23:05:35 +0100

    Added tribal music to Storyboard + fade-in in MainScene

---
+++ Assets/DestroyAfterTimeout.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTimeout : MonoBehaviour
+{
+    [SerializeField] private float _timeout = 1f;
+    private float _timer;
+
+    void Start()
+    {
+        _timer = _timeout;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        _timer -= Time.deltaTime;
+    }
+}
